950177a22f9a08354f2891efc4984ebc937b6489
Julien Dargelos <juliendargelos@users.noreply.github.com>
2024-04-30 12:29:18 +0200

    style: remove method parenthesis spaces

--- src/duration-estimator.ts
+++ src/duration-estimator.ts
@@ -4,29 +4,29 @@ export class DurationEstimator {
   public length: number
   public time?: number
 
-  public constructor (length = 10) {
+  public constructor(length = 10) {
     this.length = length
   }
 
-  public get speed (): number {
+  public get speed(): number {
     const length = this.samples.length
     return length
       ? this.samples.reduce((speed, sample) => speed + sample) / length
       : 0
   }
 
-  public get estimation (): number {
+  public get estimation(): number {
     return (1 - this.progress) / this.speed
   }
 
-  public reset (): this {
+  public reset(): this {
     this.progress = 0
     this.time = undefined
     this.samples.splice(0)
     return this
   }
 
-  public update (progress: number): this {
+  public update(progress: number): this {
     if (this.time === undefined) {
       this.progress = progress
       this.time = Date.now()
@@ -44,7 +44,7 @@ export class DurationEstimator {
     return this
   }
 
-  public estimate (progress: number): number {
+  public estimate(progress: number): number {
     return this.update(progress).estimation
   }
 }
